28aed9db3b7ba7031e176bccabd41d2fb1b74a89
ryantobin77 <ryantobin77@gmail.com>
2021-09-12 15:06:32 -0400

    Django API for user postings

--- django_api/TestDjangoApp/api/admin.py
+++ django_api/TestDjangoApp/api/admin.py
@@ -1,3 +1,4 @@
+from .models import UserPosting
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(UserPosting)
--- django_api/TestDjangoApp/api/urls.py
+++ django_api/TestDjangoApp/api/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('test', views.test_api, name='test'),
     path('register', views.register_user, name='register-user'),
     path('login', views.login_user, name='login-user'),
+    path('posting/create', views.create_user_posting, name='create-posting'),
+    path('postings/all', views.get_all_user_postings, name='get-postings'),
+    path('posting/upvote', views.upvote_posting, name='upvote-posting'),
+    path('posting/downvote', views.downvote_posting, name='downvote-posting'),
+    path('posting', views.get_posting, name='get-posting'),
 ]
